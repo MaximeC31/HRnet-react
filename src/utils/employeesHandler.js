201b58358effa1d0f218becdee0dd4ecf3ddd0ca
Maxime CHAMPIGNEUX <contact@maximechampigneux.fr>
2025-05-07 09:52:47 +0200

    localStorage changes

--- src/utils/employeesHandler.js
+++ src/utils/employeesHandler.js
@@ -1,10 +1,6 @@
 export const createEmployee = (employee) => {
   const employees = JSON.parse(localStorage.getItem("employees")) || [];
-
-  for (let i = 0; i < 5; i++) {
-    employees.push(employee);
-  }
-
+  employees.push(employee);
   localStorage.setItem("employees", JSON.stringify(employees));
   return true;
 };
